80af6173618a3c6034117a4d91e5eedb597b1c6e
nickl103 <nickl103@umn.edu>
2024-02-19 07:47:25 -0600

    data import

---
+++ R/week5.R
@@ -0,0 +1,9 @@
+#Script Setting and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+
+#Data Import
+Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+Anotes_tbl <- read_csv("../data/Anotes.csv")
+Bdata_tbl <- read_delim("../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"))
+Bnotes_tbl <- read_delim("../data/Bnotes.txt")
