5db24a34d702f77b1a581c2ab8021cdc46e3828c
nickl103 <nickl103@umn.edu>
2024-02-19 07:53:51 -0600

    simplifying code for Bdata

--- R/week5.R
+++ R/week5.R
@@ -5,5 +5,8 @@ library(tidyverse)
 #Data Import
 Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 Anotes_tbl <- read_csv("../data/Anotes.csv")
-Bdata_tbl <- read_delim("../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"))
+Bdata_tbl <- read_delim("../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q", 1:10)))
 Bnotes_tbl <- read_delim("../data/Bnotes.txt")
+
+
+  
